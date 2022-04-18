355ff40d094cbfacb269d6b3c3893fb229c54e3e
guilhermeberanger <guilherme.sberanger@gmail.com>
2022-04-18 16:00:57 -0300

    deploy Cadastro Fornecedor

--- app/configs/config.js
+++ app/configs/config.js
@@ -1,13 +1,13 @@
 const rp = require('request-promise-native');
 require('dotenv').config();
 
-const api = (body, method, oauth) => {
+const api = (processo, body, method, oauth) => {
 
     const CONSUMER_KEY = process.env.CONSUMER_KEY
     const CONSUMER_SECRET = process.env.CONSUMER_SECRET
     const TOKEN = process.env.TOKEN
     const TOKEN_SECRET = process.env.TOKEN_SECRET
-    const url = 'https://fluig.grupotb.com.br/process-management/api/v2/processes/CadastroProduto/start'
+    const url = `https://fluig.grupotb.com.br/process-management/api/v2/processes/${processo}/start`
     let options = {
         method: method,
         body: body,
--- app/routes/router.js
+++ app/routes/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path')
-const api = require('../configs/config')
+const api = require('../configs/config');
+const { urlencoded } = require('express');
 
 //configuração URL enconded
-const urlencodedParser = express.urlencoded({extended : false})
+const urlencodedParser = express.urlencoded({ extended: false })
 
 //configuração Router's
 router.get('/', (req, res) => {
@@ -19,13 +20,14 @@ router.get('/fornecedor', (req, res) => {
     res.render('fornecedor', { sucess: '' })
 })
 
-//requisição APIFLUIG
+
 /*
 Criação do Objeto FLUIG: TargetState = Numero da atividade no fluxo;
 TargetAssignee = Matrícula do USER Destino
 FormFields = DataForm Fluig
 */
-router.post('/', urlencodedParser, async (req, res) => {
+//requisição APIFLUIG
+router.post('/produdos', urlencodedParser, async (req, res) => {
     try {
         const data = {
             "targetState": "09",
@@ -45,7 +47,8 @@ router.post('/', urlencodedParser, async (req, res) => {
                 "subGrupo": req.body.subGrupo
             }
         }
-        const login = await api(data, "POST")
+        const processoProduto = `CadastroProduto`
+        const login = await api(processoProduto, data, "POST")
         const response = login.processInstanceId
         res.render('produtos', { sucess: `Cadastro Solicitado: ${response}` })
     } catch (error) {
@@ -54,4 +57,49 @@ router.post('/', urlencodedParser, async (req, res) => {
     }
 });
 
+
+router.post('/fornecedor', urlencodedParser, async (req, res) => {
+    try {
+        const dataForn = {
+            "targetState": "09",
+            "targetAssignee": "945436",
+            "subProcessTargetState": 0,
+            "comment": "",
+            "formFields": {
+                "nomeSolicitante": req.body.nomeSolicitante,
+                "emailSolicitante": req.body.emailSolicitante,
+                "tipoCadastro": req.body.tipoCadastro,
+                "reembolso": req.body.reembolso,
+                "documento": req.body.documento,
+                "enderecoCompleto": req.body.enderecoCompleto,
+                "bairro": req.body.bairro,
+                "estado": req.body.estado,
+                "cidade": req.body.cidade,
+                "cep": req.body.cep,
+                "ddd": req.body.ddd,
+                "telefone": req.body.telefone,
+                "inscricaoMunicipal": req.body.inscricaoMunicipal,
+                "inscricaoEstadual": req.body.inscricaoEstadual,
+                "emailFornecedor": req.body.emailFornecedor,
+                "contato": req.body.contato,
+                "departamento": req.body.departamento,
+                "cotacao": req.body.cotacao,
+                "formPagamento": req.body.formPagamento,
+                "banco": req.body.banco,
+                "OutroBanco": req.body.OutroBanco,
+                "codAgencia": req.body.codAgencia,
+                "codConta": req.body.codConta,
+                "codDigito": req.body.codDigito,
+                "categoria": req.body.categoria,
+                "segmento": req.body.segmento,
+            }
+        }
+        const processoFornecedor = `SolicitarCadastro`
+        const login = await api(processoFornecedor, dataForn, "POST")
+        const response = login.processInstanceId
+        res.render('fornecedor', { sucess: `Cadastro Solicitado: ${response}` })
+    } catch (error) {
+
+    }
+})
 module.exports = router
